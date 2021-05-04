5d49e7f14e779e4aef74e6775e1cfcf3509c4e1e
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-05 03:47:20 +0530

    Completed All CRUD Operations.

--- app.js
+++ app.js
@@ -49,6 +49,36 @@ app.post('/api/v1/tours', (req, res) => {
     res.send('done');
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Update successful'
+        }
+    });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+
+});
+
 app.get('/test', (req, res) => {
    res.status(200).json({message: 'Hello from the server side'});
 });
