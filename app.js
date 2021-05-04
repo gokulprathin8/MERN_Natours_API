1b36269471184c91f7dcb7f9624ee238047a67f7
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-05 03:34:14 +0530

    Added multiple details endpoints

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 const port = 3000;
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/data.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+   res.status(200).json({
+       status: 'success',
+       results: tours.length,
+       data: {
+           tours
+       }
+   })
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1
+    const tour = tours.find((element) => element.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            message: 'no data found'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/data.json`, JSON.stringify(tours), err => {
+       res.status(201);
+    });
+
+    res.send('done');
+});
 
 app.get('/test', (req, res) => {
    res.status(200).json({message: 'Hello from the server side'});
