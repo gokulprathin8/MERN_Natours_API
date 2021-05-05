b141e6d653fc87364d5fcfbd111001bf076f73ce
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-05 06:09:01 +0530

    Added Mongodb

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
 const port = process.env.PORT ||  3000;
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then((conn) => {
+    console.log(conn.connections);
+})
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
