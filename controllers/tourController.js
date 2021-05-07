8fd0a32303d1bd8d056cd6f7881c9dfc084dc0df
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-07 06:14:57 +0530

    Added Sorting, Field Limiting, Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,46 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+
+        const queryObject = {...req.query};
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(element => delete queryObject[element]);
+
+        let queryStr = JSON.stringify(queryObject);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // Sort
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy);
+        }
+
+        // Field Limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v')
+        }
+
+        // Pagination
+        const page = req.query.page * 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip > numTours) {
+                return res.status(200).json({
+                    message: "No Content Found on this Page."
+                });
+            }
+        }
+
+        const tours = await query;
 
         res.status(200).json({
             status: 'success',
@@ -16,7 +55,6 @@ exports.getAllTours = async (req, res) => {
             message: err
         })
     }
-
 }
 
 exports.getTourById = async (req, res) => {
