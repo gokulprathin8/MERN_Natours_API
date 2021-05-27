35a27100d874ffb1f2e4fd1ea57e1ee03426f69b
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-27 21:30:29 +0530

    Added Recover Password By Reset - Email sent to registered email address

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const User = require('../models/userModel');
@@ -117,6 +118,21 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     }
 });
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync(async  (req, res, next) => {
+    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({ passwordResetToken: hashedToken, passwordResetExpires: { $gt: Date.now() }});
 
-}
+    if (!user) return next(new AppError('Token is invalid or has expired', 400))
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token: token
+    })
+});
+
